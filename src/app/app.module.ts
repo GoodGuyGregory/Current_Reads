43490a3abbaea4f8f900e73e32b2ba5d9482ee13
Greg Witt <greg.witt@avnet.com>
2022-04-22 02:10:30 -0400

    Initial commit for front end of project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PrimengModule } from './modules/primeng.module';
+import { CdkVirtualScrollViewport } from '@angular/cdk/scrolling';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +13,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PrimengModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modules/primeng.module.ts
@@ -0,0 +1,21 @@
+
+import { NgModule } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { TabViewModule } from 'primeng/tabview';
+import { DataViewModule } from 'primeng/dataview';
+import { ProgressBarModule } from 'primeng/progressbar';
+import { TableModule } from 'primeng/table';
+
+const PrimeNGComponents = [
+  DataViewModule,
+  ButtonModule,
+  TabViewModule,
+  ProgressBarModule,
+  TableModule,
+];
+
+@NgModule({
+  imports: [PrimeNGComponents],
+  exports: [PrimeNGComponents]
+})
+export class PrimengModule { }
